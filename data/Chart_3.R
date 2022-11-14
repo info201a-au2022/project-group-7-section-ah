7ef026dd7ee0a4e098a606c96589dd4aef53ef87
erihuynh <erihuynh@uw.edu>
2022-11-13 20:07:37 -0800

    updated chart

--- data/Chart_3.R
+++ data/Chart_3.R
@@ -20,13 +20,13 @@ preg_df <- abortion %>%
             pregnancyrate3034,
             pregnancyrate3539,
             pregnancyrate40plus)
-  
-  
+
+
 #average pregnancy rates by age
 avg_preg_lt15 <- round(mean(abortion$pregnancyratelt15, na.rm = T), digits = 2)
 
 avg_preg_1517 <- round(mean(abortion$pregnancyrate1517, na.rm = T), digits = 2)
-  
+
 avg_preg_1819 <- round(mean(abortion$pregnancyrate1819, na.rm = T), digits = 2)
 
 avg_preg_2024 <- round(mean(abortion$pregnancyrate2024, na.rm = T), digits = 2)
@@ -49,7 +49,7 @@ avg_preg_df <- data.frame(avg_preg_lt15,
                           avg_preg_3034,
                           avg_preg_3539,
                           avg_preg_40plus
-                          )
+)
 
 
 #dataframe wrangling
@@ -70,5 +70,25 @@ print(pregnancy_rates)
 
 
 #scatterplot
-ggplot(pregnancy_rates, aes(x = age_range, y = pregnancy_rates)) + geom_point()
+ggplot(pregnancy_rates, 
+       aes(x = age_range, 
+           y = pregnancy_rates)) + 
+  geom_point(color = "black") +
+  geom_point(data = pregnancy_rates, aes(color = "Average")) +
+  geom_point(data = pregnancy_rates, aes(color = "Individual Rates")) +
+  labs(color = "Key", x = "Age Ranges", y = "Pregnancy Rate (percentage)") +
+  scale_color_manual(values = c("red", "black")) +
+  geom_point(aes(x = 1,y = avg_preg_lt15), color = "red") +
+  geom_point(aes(x = 2,y = avg_preg_1517), color = "red") +
+  geom_point(aes(x = 3,y = avg_preg_1819), color = "red") +
+  geom_point(aes(x = 4,y = avg_preg_2024), color = "red") +
+  geom_point(aes(x = 5,y = avg_preg_2529), color = "red") +
+  geom_point(aes(x = 6,y = avg_preg_3034), color = "red") +
+  geom_point(aes(x = 7,y = avg_preg_3539), color = "red") +
+  geom_point(aes(x = 8,y = avg_preg_40plus), color = "red") +
+  ggtitle("Rate of Pregnancy Given Age") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
 
