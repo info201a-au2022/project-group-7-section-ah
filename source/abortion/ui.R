18692569827546022faedad6cf36b5fee01abf12
erihuynh <erihuynh@uw.edu>
2022-12-01 16:09:22 -0800

    new stuff!

--- source/abortion/ui.R
+++ source/abortion/ui.R
@@ -1,61 +1,130 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+############################################
+##### P3 : POST-ROE IMPACTS ON AMERICA #####
+######### INFO 201 - AH - GROUP 7 ##########
+############################################
 
-library(shiny)
+# set- up
+library("shiny")
+library("shinythemes")
+library("tidyverse")
+library("data.table")
+library("ggplot2")
+library("plotly")
+library("bslib")
 
-# Define UI for application that draws a histogram
+abortion <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ah/main/data/NationalAndStatePregnancy_PublicUse.csv")
+
+
+##############################
+##### home_page variable #####
+##############################
+home_page <- tabPanel("Home", 
+                      hr(),
+                      sidebarLayout(
+                        sidebarPanel(
+                          h1("About Us"),
+                          hr(),
+                          
+                          h5("Erica Huynh"),
+                          p("she/her"),
+                          p("erihuynh@uw.edu"),
+                          br(),
+                          
+                          h5("Michelle Kim"),
+                          p("she/her"),
+                          p("mkim45@uw.edu"),
+                          br(),
+                          
+                          h5("Jasmine Vuong"),
+                          p("she/her"),
+                          p("jasvu@uw.edu"),
+                          br(),
+                          
+                          h4("INFO 201 Fall 2022"),
+                        ),
+                        
+                        
+                        mainPanel(
+                          br(),
+                          h1("Our Mission"),
+                          p("Throughout this project, our group aimed to address the 
+                          effectiveness of the court case Roe v. Wade in 1973 given 
+                          its overturn this year. With many states in America deciding 
+                          to restrict access to abortions, it becomes important to 
+                          analyze how safe abortions have benefited people because 
+                          of this case."),
+                          
+                          h4("Research Questions"),
+                          p("Our research questions addressed how birthrates and abortion 
+                          rates have changed after the passing of Roe, how abortion 
+                          rates have changed between states with differing political
+                          views, and how pregnancy rates between different age groups 
+                          have been impacted since Roe."),
+                          
+                          h4("Analyzed Data"),
+                          p("The dataset we worked with included information from 1973
+                          to 2017 concerning trends of pregnancies, abortions, 
+                          birthrates, etc. We focused on the rates of these categories 
+                          among states and ages in order to fully understand Roe’s
+                          impact on our current society."),
+                          
+                          h4("Key Findings"),
+                          p("It was found that there were declining abortion rates even
+                          before Roe’s overturn, with a peak after it was passed. 
+                          Due to the recession in 2008, birth rates increased while 
+                          abortion rates decreased. People in their early to mid 
+                          twenties tended to have higher pregnancy rates, possibly 
+                          due to other age ranges seeking contraceptive measures 
+                          instead due to the correlation between age and societal 
+                          expectations. Ultimately, within the data we analyzed, 
+                          we concluded that more abortion restrictions may lead to 
+                          higher birth and pregnancy rates, leading to lower abortion 
+                          rates due to the limited access across America, especially 
+                          in abortion-restricted states."),
+                          
+                        )
+                      )
+)
+
+
+########################################
+##### abt_birth_page variable #####
+########################################
+abt_birth_page <- tabPanel("Abortion & Birth Rates")
+
+
+##########################################
+##### abt_rate_compare_page variable #####
+##########################################
+abt_rate_compare_page <- tabPanel("Abortion Rate Comparisons")
+
+
+#######################################
+##### preg_rate_age_page variable #####
+#######################################
+preg_rate_age_page <- tabPanel("Pregnancy Rates & Age")
+
+
+######################################
+##### final_report_page_variable #####
+######################################
+final_report_page <- tabPanel("Final Report",
+                              h1("Summary"))
+
+
+
+######################
+##### defined ui #####
+######################
 ui <- fluidPage(
+  theme = bs_theme(bootswatch = "lux"),
+  
+  img(src="header.jpg", height="110%", width="100%"),
   navbarPage(title = "Post-Roe Impacts on America",
-             tabPanel("Our Mission", 
-                      h1("Our Mission"),
-                      
-                      HTML('<center><img src="abortion_photo.jpg" width="300"></center>'),
-                      
-                      h3("Problem Domain"),
-                      p("Throughout this project, our group aimed to address the 
-                        effectiveness of the court case Roe v. Wade in 1973 given 
-                        its overturn this year. With many states in America deciding 
-                        to restrict access to abortions, it becomes important to 
-                        analyze how safe abortions have benefited people because 
-                        of this case."),
-                      
-                      h3("Research Questions"),
-                      p("Our research questions addressed how birthrates and abortion 
-                        rates have changed after the passing of Roe, how abortion 
-                        rates have changed between states with differing political
-                        views, and how pregnancy rates between different age groups 
-                        have been impacted since Roe."),
-                      
-                      h3("Analyzed Data"),
-                      p("The dataset we worked with included information from 1973
-                        to 2017 concerning trends of pregnancies, abortions, 
-                        birthrates, etc. We focused on the rates of these categories 
-                        among states and ages in order to fully understand Roe’s
-                        impact on our current society."),
-                      
-                      h3("Key Findings"),
-                      p("It was found that there were declining abortion rates even
-                        before Roe’s overturn, with a peak after it was passed. 
-                        Due to the recession in 2008, birth rates increased while 
-                        abortion rates decreased. People in their early to mid 
-                        twenties tended to have higher pregnancy rates, possibly 
-                        due to other age ranges seeking contraceptive measures 
-                        instead due to the correlation between age and societal 
-                        expectations. Ultimately, within the data we analyzed, 
-                        we concluded that more abortion restrictions may lead to 
-                        higher birth and pregnancy rates, leading to lower abortion 
-                        rates due to the limited access across America, especially 
-                        in abortion-restricted states."),
-                      ),
-             tabPanel("Summary", "LOL"),
-             tabPanel("Birth & Abortion Rates", "Hello"),
-             tabPanel("Total Abortion Rate Comparisons", "Hello again"),
-             tabPanel("Pregnancy Rates & Age", "HELLOOOO"),
-             tabPanel("Final Report", "HIIII")
-  ))
+             home_page,
+             abt_birth_page,
+             abt_rate_compare_page,
+             preg_rate_age_page,
+             final_report_page
+  )
+)
